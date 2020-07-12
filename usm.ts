8cfab3c81ff3e9de6e2cab57931dd958bd06d33b
MisakaHonoka <2361547758@qq.com>
2020-07-12 22:54:07 +0800

    init

---
+++ usm.ts
@@ -0,0 +1,220 @@
+class CRID {
+    _vm1: Uint8Array; // _video_vm11
+    _vm2: Uint8Array; // _video_vm12
+    _am:  Uint8Array; // _audioMask
+    video: Uint8Array;
+    audio: Uint8Array;
+
+    parseKey (key:any) {
+        let buff = new Uint8Array(4);
+        try { switch (typeof key) {
+            case "string":
+                key = parseInt(key)
+            case 'number':
+                buff[0] = key & 0xff;
+                buff[0] = key >> 8 & 0xff;
+                buff[0] = key >> 16 & 0xff;
+                buff[0] = key >> 24 & 0xff;
+                break;
+            case 'object':
+                if (key instanceof Uint8Array) return key;
+        } } catch {
+            // key parse error
+        } finally {
+            return buff;
+        }
+    }
+
+    constructor (key1:any = null, key2:any = 0) {
+        this._vm1 = new Uint8Array(0x20);
+        this._vm2 = new Uint8Array(0x20);
+        this._am  = new Uint8Array(0x20);
+        if (key1 === null) {
+            key1 = this.parseKey(0x43484A86);
+            key2 = this.parseKey(0x00000001);
+        } else if (typeof key1 === 'number') {
+            key1 = this.parseKey(key1);
+            key2 = this.parseKey(key2 ? key2 : key1 >> 32);
+        } else if (typeof key1 === 'string') {
+            key1 = parseInt(key1);
+            key1 = this.parseKey(key1);
+            key2 = this.parseKey(key2 ? key2 : key1 >> 32);
+        } else {
+            key1 = this.parseKey(key1);
+            key2 = this.parseKey(key2);
+        }
+        this.initMask(key1, key2);
+    }
+
+    initMask (key1:Uint8Array, key2:Uint8Array) {
+        this._vm1[0x00] = key1[0];
+        this._vm1[0x01] = key1[1];
+        this._vm1[0x02] = key1[2];
+        this._vm1[0x03] = key1[3] - 0x34;
+        this._vm1[0x04] = key2[0] + 0xF9;
+        this._vm1[0x05] = key2[1] ^ 0x13;
+        this._vm1[0x06] = key2[2] + 0x61;
+        this._vm1[0x07] = this._vm1[0x00] ^ 0xFF;
+        this._vm1[0x08] = this._vm1[0x02] + this._vm1[0x01];
+        this._vm1[0x09] = this._vm1[0x01] - this._vm1[0x07];
+        this._vm1[0x0A] = this._vm1[0x02] ^ 0xFF;
+        this._vm1[0x0B] = this._vm1[0x01] ^ 0xFF;
+        this._vm1[0x0C] = this._vm1[0x0B] + this._vm1[0x09];
+        this._vm1[0x0D] = this._vm1[0x08] - this._vm1[0x03];
+        this._vm1[0x0E] = this._vm1[0x0D] ^ 0xFF;
+        this._vm1[0x0F] = this._vm1[0x0A] - this._vm1[0x0B];
+        this._vm1[0x10] = this._vm1[0x08] - this._vm1[0x0F];
+        this._vm1[0x11] = this._vm1[0x10] ^ this._vm1[0x07];
+        this._vm1[0x12] = this._vm1[0x0F] ^ 0xFF;
+        this._vm1[0x13] = this._vm1[0x03] ^ 0x10;
+        this._vm1[0x14] = this._vm1[0x04] - 0x32;
+        this._vm1[0x15] = this._vm1[0x05] + 0xED;
+        this._vm1[0x16] = this._vm1[0x06] ^ 0xF3;
+        this._vm1[0x17] = this._vm1[0x13] - this._vm1[0x0F];
+        this._vm1[0x18] = this._vm1[0x15] + this._vm1[0x07];
+        this._vm1[0x19] = 0x21            - this._vm1[0x13];
+        this._vm1[0x1A] = this._vm1[0x14] ^ this._vm1[0x17];
+        this._vm1[0x1B] = this._vm1[0x16] + this._vm1[0x16];
+        this._vm1[0x1C] = this._vm1[0x17] + 0x44;
+        this._vm1[0x1D] = this._vm1[0x03] + this._vm1[0x04];
+        this._vm1[0x1E] = this._vm1[0x05] - this._vm1[0x16];
+        this._vm1[0x1F] = this._vm1[0x1D] ^ this._vm1[0x13];
+
+        const t2 = 'URUC';
+        for(let i = 0; i < 0x20; i++) {
+            this._vm2[i] = this._vm1[i] ^ 0xFF;
+            this._am[i]  = (i & 1) ? t2[(i >> 1) & 3].charCodeAt(0) : this._vm2[i];
+        }
+    }
+
+    async demuxAsync(data: ArrayBuffer) {
+        let ftell = 0;
+        let v_trunks:Promise<Uint8Array>[] = [], a_trunks:Promise<Uint8Array>[] = [];
+        let v_size = 0, a_size = 0;
+        while (ftell < data.byteLength) {
+            const fp = new DataView(data, ftell, 16);
+            const magic = fp.getUint32(0, true);
+            const len = fp.getUint32(4);
+            const off  = fp.getUint16(8);
+            const pad = fp.getUint16(10);
+            const type = fp.getUint32(12);
+            let p:Uint8Array, mask:Uint8Array;
+            switch (magic) {
+                case 0x44495243: // CRID
+                    p = new Uint8Array(data, ftell + off + 8, len - off - pad);
+                    break;
+                case 0x56465340: // @SFV
+                    if (type) break;
+                    data.slice(ftell + off + 8, )
+                    p = new Uint8Array(data, ftell + off + 8, len - off - pad);
+                    v_size += p.byteLength;
+                    v_trunks.push(this.maskVideoAsync(p))
+                    break;
+                case 0x41465340: // @SFA
+                    if (type) break;
+                    p = new Uint8Array(data, ftell + off + 8, len - off - pad);
+                    a_size += p.byteLength;
+                    a_trunks.push(this.maskAudioAsync(p))
+                    break;
+                default: // @CUE ...
+            }
+            ftell += len + 8;
+        }
+        this.video = new Uint8Array(v_size);
+        this.audio = new Uint8Array(a_size);
+        await Promise.all([
+            this.concatAsync(v_trunks, "video"),
+            this.concatAsync(a_trunks, "audio")
+        ])
+        return {
+            video: this.video,
+            audio: this.audio
+        }
+    }
+    demux(data: ArrayBuffer) {
+        let ftell = 0;
+        let v_trunks:Uint8Array[] = [], a_trunks:Uint8Array[] = [];
+        let v_size = 0, a_size = 0;
+        while (ftell < data.byteLength) {
+            const fp = new DataView(data, ftell, 16);
+            const magic = fp.getUint32(0, true);
+            const len = fp.getUint32(4);
+            const off  = fp.getUint16(8);
+            const pad = fp.getUint16(10);
+            const type = fp.getUint32(12);
+            let p:Uint8Array, mask:Uint8Array;
+            switch (magic) {
+                case 0x44495243: // CRID
+                    p = new Uint8Array(data, ftell + off + 8, len - off - pad);
+                    break;
+                case 0x56465340: // @SFV
+                    if (type) break;
+                    data.slice(ftell + off + 8, )
+                    p = new Uint8Array(data, ftell + off + 8, len - off - pad);
+                    v_size += p.byteLength;
+                    v_trunks.push(this.maskVideo(p))
+                    break;
+                case 0x41465340: // @SFA
+                    if (type) break;
+                    p = new Uint8Array(data, ftell + off + 8, len - off - pad);
+                    a_size += p.byteLength;
+                    a_trunks.push(this.maskAudio(p))
+                    break;
+                default: // @CUE ...
+            }
+            ftell += len + 8;
+        }
+        this.video = new Uint8Array(v_size);
+        this.audio = new Uint8Array(a_size);
+        this.concat(v_trunks, "video")
+        this.concat(a_trunks, "audio")
+        return {
+            video: this.video,
+            audio: this.audio
+        }
+    }
+    async maskVideoAsync(p:Uint8Array) {
+        if (p.byteLength >= 576) {
+            let mask = new Uint8Array(this._vm2);
+            for (let j = 320; j < p.byteLength; j++) mask[j & 31] = p[j] ^= mask[j & 31];
+            mask = new Uint8Array(this._vm1);
+            for (let j = 64; j < 320; j++) p[j] ^= mask[j & 31] ^= p[j + 256];
+        }
+        return p;
+    }
+    maskVideo(p:Uint8Array) {
+        if (p.byteLength >= 576) {
+            let mask = new Uint8Array(this._vm2);
+            for (let j = 320; j < p.byteLength; j++) mask[j & 31] = p[j] ^= mask[j & 31];
+            mask = new Uint8Array(this._vm1);
+            for (let j = 64; j < 320; j++) p[j] ^= mask[j & 31] ^= p[j + 256];
+        }
+        return p;
+    }
+    async maskAudioAsync(p:Uint8Array) {
+        for (let j = 320; j < p.byteLength; j++) p[j] ^= this._am[j & 31];
+        return p;
+    }
+    maskAudio(p:Uint8Array) {
+        for (let j = 320; j < p.byteLength; j++) p[j] ^= this._am[j & 31];
+        return p;
+    }
+    private async concatAsync(trunks: Promise<Uint8Array>[], type: string) {
+        let dist = this[type]
+        let offset = 0;
+        for (let i of trunks) {
+            let buff = await i;
+            dist.set(buff, offset);
+            offset += buff.byteLength;
+        }
+    }
+    private concat(trunks: Uint8Array[], type: string) {
+        let dist = this[type]
+        let offset = 0;
+        for (let i of trunks) {
+            let buff = i;
+            dist.set(buff, offset);
+            offset += buff.byteLength;
+        }
+    }
+}
